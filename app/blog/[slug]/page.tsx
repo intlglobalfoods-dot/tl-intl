0086bfd6f03683208b08281830afda4a010e03cb
intlglobalfoods-dot <intl.globalfoods@gmail.com>
2026-03-31 03:16:23 +0530

    Create individual blog post page for slug

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Post = () => {
+    return (
+        <div>
+            <h1>Your Blog Post Title</h1>
+            <p>This is the full content of your blog post. Replace this text with the actual content of your article.</p>
+        </div>
+    );
+};
+
+export default Post;
